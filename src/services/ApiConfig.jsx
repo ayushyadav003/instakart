bd46cdfd91dbf2cdf6c74a892406e71ec3725b23
Ayush <ayush.yadav.100100@gmail.com>
2025-03-10 17:59:26 +0530

    update

--- src/services/ApiConfig.jsx
+++ src/services/ApiConfig.jsx
@@ -1,8 +1,11 @@
-const apiUrl = 'https://instakart-backend.onrender.com/api'
-// const apiUrl = 'http://localhost:5000'
+// const apiUrl = 'https://instakart-backend.onrender.com/api'
+const apiUrl = 'http://localhost:5000'
 
 export const apiConfig = {
   //auth
   signup: `${apiUrl}/v1/auth/signup`,
   login: `${apiUrl}/v1/auth/login`,
+
+  //products
+  getProductList: `${apiUrl}/v1/auth/products`,
 }
